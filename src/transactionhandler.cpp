9de4e51143251e498867e0f6e23aa4f8810eaf1a
Wouter Bruggeman <wouterbruggeman@live.nl>
2018-03-29 15:44:20 +0000

    Change transactionhandler

--- src/transactionhandler.cpp
+++ src/transactionhandler.cpp
@@ -5,15 +5,132 @@
 TransactionHandler::TransactionHandler(Db *databasePtr){
 	database = databasePtr;
 
+	//Create a timer
 	timer = new QTimer(this);
-	connect(timer, SIGNAL(timeout()), this, SLOT(handlePending()));
+	connect(timer, SIGNAL(timeout()), this, SLOT(cycle()));
 	
+	//Set the timer
 	Config c(databasePtr);
 	int timerDelay = c.getValue("transaction_delay").toInt();
-
 	timer->start(timerDelay);
+
+	//Set own iban prefix
+	ibanPrefix = c.getValue("iban_prefix").toString();
+	
+	receiveServerList();
 }
 
-void TransactionHandler::handlePending(){
-	qDebug() << "Timer test";
+void TransactionHandler::cycle(){
+	//handle transactions for the selected server.
+	handleTransactions(serverList.at(serverIterator));
+
+	serverIterator++;
+	if(serverIterator == serverList.size()){
+		serverIterator = 0;
+
+		//Reload the server list.
+		receiveServerList();
+		
+		//Handle transactions for our own server.
+		server s;
+		s.prefix = ibanPrefix;
+		handleTransactions(s);
+	}
+}
+
+void TransactionHandler::receiveServerList(){
+	//Clear the server list
+	serverList.clear();
+
+	//Receive the server list
+	QString sql = "SELECT * from iban_ip_link";
+	QSqlQuery query = database->q(sql, {});
+	query.exec();
+	if(!query.isActive()){
+		return;
+	}
+
+	//Loop through all received data
+	while(query.next()){
+		server s;
+		s.prefix = query.value(0).toString();
+		s.ip = query.value(1).toString();
+		s.portnr = query.value(2).toInt();
+
+		//Add server struct to the QVector
+		serverList.append(s);
+	}
+}
+
+bool TransactionHandler::pingServer(QString ip){
+	if(ip == ""){
+		return false;
+	}
+
+	//Method below works, but spams the console.
+	/*QStringList parameters;
+	#if defined(WIN32)
+		parameters << "-n" << "1";
+	#else
+		parameters << "-c 1";
+	#endif
+	parameters << ip;
+	int exitCode = QProcess::execute("ping", parameters);
+	return !exitCode;*/
+
+	//TODO: Implement a better way.
+	return true;
+}
+
+void TransactionHandler::handleTransactions(server s){
+	bool isOwnServer = (s.prefix == ibanPrefix);
+	bool serverIsOnline = pingServer(s.ip);
+
+	//If the server is not our own and is offline, return;
+	if(!serverIsOnline && !isOwnServer){
+		return;
+	}
+
+	//cycle through all transactions where the status is not accepted for the specified bank.
+	QString sql = "SELECT * FROM transactions WHERE to_iban LIKE '%?%' AND status <> ?";
+	QSqlQuery query = database->q(sql, {s.prefix, STATUS_DONE});
+	query.exec();
+	while(query.next()){
+		//Internal transaction.
+		if(query.value(1) == query.value(2)){
+			internalTransfer(query.value(1).toString(),
+					query.value(2).toString(), query.value(3).toInt());
+			setTransactionStatus(query.value(0).toInt(), STATUS_DONE);
+		}else{
+			//TODO: Implement external transaction system.
+		}
+	}
+}
+
+void TransactionHandler::internalTransfer(QString from_iban, QString to_iban, int amount){
+	//If the account has enough balance
+	QString sql = "SELECT balance FROM account WHERE iban = ?";
+	QSqlQuery query = database->q(sql, {from_iban});
+	query.exec();
+	if(!query.isActive()){
+		return;
+	}
+	if(!query.first()){
+		return;
+	}
+	
+	if(query.value(0) >= amount){
+		//Transfer the money
+		QString sql = "UPDATE account SET balance = (balance - ?) WHERE iban = ?;\
+			       UPDATE account SET balance = (balance + ?) WHERE iban = ?;";
+		QSqlQuery query = database->q(sql, {amount, from_iban, amount, to_iban});
+		query.exec();
+	}
+	return;
+}
+
+void TransactionHandler::setTransactionStatus(int transaction, int status){
+	QString sql = "UPDATE transaction SET status = ? WHERE id = ?";
+	QSqlQuery query = database->q(sql, {status, transaction});
+	query.exec();
 }
--- src/transactionhandler.h
+++ src/transactionhandler.h
@@ -3,7 +3,20 @@
 
 #include <QObject>
 #include <QTimer>
+#include <QString>
+#include <QVector>
 #include "db.h"
+
+#define STATUS_PENDING 0
+#define STATUS_DONE 1
+
+
+
+struct server{
+	QString prefix;
+	QString ip;
+	int portnr;
+};
 
 class TransactionHandler : public QObject{
 	Q_OBJECT
@@ -12,10 +25,21 @@ class TransactionHandler : public QObject{
 		TransactionHandler(Db *databasePtr);
 
 	public slots:
-		void handlePending();
+		void cycle();
 		
 	private:
-		QTimer *timer;	
+		//Functions
+		void receiveServerList();
+		bool pingServer(QString ip);
+		void handleTransactions(server s);
+		void internalTransfer(QString from_iban, QString to_iban, int amount);
+		void setTransactionStatus(int transaction, int status);
+
+		//Variables
+		QVector<server> serverList;
+		int serverIterator = 0;
+		QString ibanPrefix;
+		QTimer *timer;
 		Db *database;
 };
 #endif
